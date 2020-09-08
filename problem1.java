214a1b6a22e03eae70fe88872a78e7e27c9dc534
firefistacez <fansforever711@gmail.com>
2020-09-08 22:56:16 +0530

    Java program to find the answer to the question given in README.MD file

---
+++ problem1.java
@@ -0,0 +1,13 @@
+package com.example.problem1;
+
+public class problem1 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i < 1000; i += 1) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                total += i;
+            }
+        }
+        System.out.println(total);
+    }
+}
